30dd330d75413ad5518a4cf5cb22b89893ea3122
Chris Davis <christopher.j.davis03@gmail.com>
2022-08-12 12:45:28 -0500

    initial commit

---
+++ src/main/java/com/capstone/jobtographer/JobtographerApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.jobtographer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobtographerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobtographerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/capstone/jobtographer/JobtographerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.jobtographer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobtographerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
