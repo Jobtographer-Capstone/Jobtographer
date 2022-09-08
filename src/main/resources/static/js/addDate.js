5260899a0cf126286bfb66fa8568238823228f29
orgeron.kevin <orgeron.kevin@gmail.com>
2022-09-08 16:10:46 -0500

    fixes for dates

--- src/main/resources/static/js/addDate.js
+++ src/main/resources/static/js/addDate.js
@@ -4,14 +4,23 @@ document.querySelector('.addBtn').addEventListener('click', () => {
     let dates = document.querySelectorAll('.dateValues')
     dates.forEach((date, i) => {
 
-        if (document.querySelectorAll('.testing').item(i).value !== "" && date.value === "") {
-            date.value = document.querySelectorAll('.testing').item(i).value
+        try {
+            if (document.querySelectorAll('.testing').item(i).value !== "" && date.value === "") {
+                date.value = document.querySelectorAll('.testing').item(i).value
+            }
+        } catch (e) {
+            console.log(e);
         }
-
-        if (date.value === "") {
-            date.value = "2099-01-01"
-        }
-
+try {
+    if (date.value === "") {
+        date.value = "2099-01-01"
+    }
+} catch (e) {
+    console.log(e);
+}
+        // if (document.querySelectorAll('.testing').item(i).value === null && date.value !== "") {
+        //     document.querySelector('.dates').innerHTML += `<input name="expected" type="hidden" value="${date.value}"/>`
+        // }
         document.querySelector('.dates').innerHTML += `<input name="expected" type="hidden" value="${date.value}"/>`
     })
     let f = document.querySelector('.dateForm')
